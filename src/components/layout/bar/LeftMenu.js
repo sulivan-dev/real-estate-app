80f3211f19c729e8e44935cf41900d2e37ec90c7
sulivan-dev <wil.alvarado.dev@gmail.com>
2020-05-22 23:22:48 -0600

    LeftMenu component finished. Modifications in SessionBar component.

---
+++ src/components/layout/bar/LeftMenu.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link, List, ListItem, ListItemText, Divider } from '@material-ui/core';
+
+export const LeftMenu = ({ classes }) => (
+  <div className={ classes.list }>
+    <List>
+      <ListItem button component={ Link } to="#">
+        <i className="material-icons">account_box</i>
+        <ListItemText classes={{ primary: classes.listItemText }} primary="Perfil" />
+      </ListItem>
+    </List>
+    <Divider/>
+    <List>
+      <ListItem button component={ Link } to="#">
+        <i className="material-icons">add_box</i>
+        <ListItemText classes={{ primary: classes.listItemText }} primary="Nuevo Inmueble" />
+      </ListItem>
+      <ListItem button component={ Link } to="#">
+        <i className="material-icons">business</i>
+        <ListItemText classes={{ primary: classes.listItemText }} primary="Inmuebles" />
+      </ListItem>
+      <ListItem button component={ Link } to="#">
+        <i className="material-icons">mail_outline</i>
+        <ListItemText classes={{ primary: classes.listItemText }} primary="Mensajes" />
+      </ListItem>
+    </List>
+  </div>
+)
--- src/components/layout/bar/SessionBar.js
+++ src/components/layout/bar/SessionBar.js
@@ -1,24 +1,25 @@
 import React, {Component} from 'react';
-import { withRouter } from 'react-router-dom';
-import { compose } from 'recompose';
-import { Toolbar, Typography, Button, IconButton, Drawer } from "@material-ui/core";
-import { withStyles } from "@material-ui/core/styles";
-import { FirebaseConsumer } from "../../../firebase";
-import { exitSession } from "../../../session/actions/sessionActions";
-import { StateContext } from "../../../session/store";
-import { RightMenu } from "./RightMenu";
+import {withRouter, Link} from 'react-router-dom';
+import {compose} from 'recompose';
+import {Avatar, Toolbar, Typography, Button, IconButton, Drawer} from "@material-ui/core";
+import {withStyles} from "@material-ui/core/styles";
+import {FirebaseConsumer} from "../../../firebase";
+import {exitSession} from "../../../session/actions/sessionActions";
+import {StateContext} from "../../../session/store";
+import {RightMenu} from "./RightMenu";
+import {LeftMenu} from "./LeftMenu";
 import Logo from '../../../logo.svg';
 
 const styles = theme => ({
   desktopSection: {
     display: 'none',
-    [ theme.breakpoints.up('md')]: {
+    [theme.breakpoints.up('md')]: {
       display: 'flex',
     }
   },
   mobileSection: {
     display: 'flex',
-    [ theme.breakpoints.up('md')]: {
+    [theme.breakpoints.up('md')]: {
       display: 'none',
     }
   },
@@ -45,23 +46,23 @@ class SessionBar extends Component {
 
   state = {
     firebase: null,
-    right: '',
-    left: '',
+    right: false,
+    left: false,
   }
 
   exitSessionMethod = () => {
-    const { firebase } = this.state;
-    const[, dispatch] = this.context;
+    const {firebase} = this.state;
+    const [, dispatch] = this.context;
 
     exitSession(dispatch, firebase)
-      .then(response => {
-        this.props.history.push('/auth/login');
-      })
+        .then(response => {
+          this.props.history.push('/auth/login');
+        })
   }
 
   toggleDrawer = (side, open) => () => {
     this.setState({
-      [side] : open,
+      [side]: open,
     })
   }
 
@@ -76,56 +77,76 @@ class SessionBar extends Component {
   }
 
   render() {
-    const { classes } = this.props;
-    const [{ session }] = this.context;
-    const { user } = session;
+    const {classes} = this.props;
+    const [{session}] = this.context;
+    const {user} = session;
     const userText = user.firstName + ' ' + user.lastName;
 
     return (
-      <div>
-        <Drawer open={ this.state.right }
-                onClose={ this.toggleDrawer('right', false) }
-                anchor="right"
-        >
-          <div role="button"
-               onClick={ this.toggleDrawer('right', false) }
-               onKeyDown={ this.toggleDrawer('right', false) }
+        <div>
+          <Drawer open={this.state.left}
+                  onClose={this.toggleDrawer('left', false)}
+                  anchor="left"
           >
-            <RightMenu classes={ styles }
-                       user={ user }
-                       userText={ userText }
-                       userPhoto={ Logo }
-                       exitSession={ this.exitSessionMethod }
-            />
-          </div>
-        </Drawer>
-        <Toolbar>
-          <IconButton color="inherit">
-            <i className="material-icons">menu</i>
-          </IconButton>
-
-          <Typography variant="h6">
-            VAXI HOMES
-          </Typography>
-
-          <div className={ classes.grow }>
-          </div>
-
-          <div className={ classes.desktopSection }>
-            <Button color="inherit">Login</Button>
-          </div>
-
-          <div className={ classes.mobileSection }>
-            <IconButton color="inherit"
-                        onClick={ this.toggleDrawer('right', true) }
+            <div role="button"
+                 onClick={this.toggleDrawer('left', false)}
+                 onKeyDown={this.toggleDrawer('left', false)}
             >
-              <i className="material-icons">more_vert</i>
+              <LeftMenu classes={styles}/>
+            </div>
+          </Drawer>
+
+          <Drawer open={this.state.right}
+                  onClose={this.toggleDrawer('right', false)}
+                  anchor="right"
+          >
+            <div role="button"
+                 onClick={this.toggleDrawer('right', false)}
+                 onKeyDown={this.toggleDrawer('right', false)}
+            >
+              <RightMenu classes={styles}
+                         user={user}
+                         userText={userText}
+                         userPhoto={Logo}
+                         exitSession={this.exitSessionMethod}
+              />
+            </div>
+          </Drawer>
+
+          <Toolbar>
+            <IconButton color="inherit"
+                        onClick={this.toggleDrawer('left', true)}>
+              <i className="material-icons">menu</i>
             </IconButton>
-          </div>
-        </Toolbar>
-      </div>
+
+            <Typography variant="h6">
+              Homes
+            </Typography>
+
+            <div className={classes.grow}>
+            </div>
+
+            <div className={classes.desktopSection}>
+              <IconButton color="inherit" component={Link} to="#">
+                <i className="material-icons">mail_outline</i>
+              </IconButton>
+              <Button color="inherit" onClick={this.exitSessionMethod}>Salir</Button>
+              <Button color="inherit">{userText}</Button>
+              <Avatar src={Logo}>
+
+              </Avatar>
+            </div>
+
+            <div className={classes.mobileSection}>
+              <IconButton color="inherit"
+                          onClick={this.toggleDrawer('right', true)}>
+                <i className="material-icons">more_vert</i>
+              </IconButton>
+            </div>
+          </Toolbar>
+        </div>
     );
   }
 }
 
-export default compose(withRouter, withStyles(styles), FirebaseConsumer) (SessionBar);
+export default compose(withRouter, withStyles(styles), FirebaseConsumer)(SessionBar);
